5897d40132bbfc2e1a8a08497681bc09c874f2ef
amankul2001 <aman@grrassolution.in>
2025-07-18 10:19:17 +0530

    completed rbac auth project

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const verifyToken = require('../middleware/authMiddleware');
+const authorizeRoles = require('../middleware/roleMiddleware');
 const router = express.Router();
 
 
-router.get('/admin', verifyToken, (req , res)=>{
+router.get('/admin', verifyToken, authorizeRoles("admin"),(req , res)=>{
         res.json({message:"Welcome Admin"})
 });
 
-router.get('/manager', (req , res)=>{
+router.get('/manager', verifyToken ,authorizeRoles("admin","manager"), (req , res)=>{
         res.json({message:"Welcome Manager"})
 });
 
-router.get('/user', (req , res)=>{
+router.get('/user',verifyToken,authorizeRoles("admin","manager","user") ,(req , res)=>{
         res.json({message:"Welcome User"})
 });
 
